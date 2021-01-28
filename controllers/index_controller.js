33fca625425e7158edc99bd3e28edf23481e6335
Daniel Adhi Nugraha <danielanugr@yahoo.co.id>
2021-01-29 00:22:44 +0700

    add login page

--- controllers/index_controller.js
+++ controllers/index_controller.js
@@ -3,6 +3,17 @@ class ControllerIndex {
     res.render("index")
     // res.send('masuk controller index')
   }
+
+  static getLogin(req, res) {
+    res.render("login")
+  }
+
+  static postLogin(req, res) {
+    let obj = {
+      username: req.body.username,
+      password: req.body.password
+    }
+  }
 }
 
 module.exports = ControllerIndex
